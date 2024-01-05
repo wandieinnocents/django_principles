243d30a137e918327fe328fc496c1ba1ecc00c82
wandieinnocent <wandieinnocent2@gmail.com>
2024-01-05 08:46:47 +0300

    Commit

---
+++ calc/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+     return render(request, 'home.html', {'name':'wandie'})
+
+def add(request):
+     num1 = int(request.POST["num1"])
+     num2 = int(request.POST["num2"])
+     res = num1 + num2 
+     return render(request,'result.html', { 'result': res })
